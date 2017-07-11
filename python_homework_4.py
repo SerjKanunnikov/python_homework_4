67e93c51e70d8d91f80c958fd446e693ebfe82ed
Kanunnikov Sergey <SergeyKanunnikov@MacBook-Pro-Kanunnikov.local>
2017-07-11 10:49:06 +0300

    Command choice, delete, move, add shelves functions

--- python_homework_4.py
+++ python_homework_4.py
@@ -9,16 +9,6 @@ directories = {
         '2': ['10006'],
         '3': []
       }
-    
-# Задача №1
-
-# Необходимо реализовать пользовательские команды, которые будут выполнять следующие функции:
-
-# p – people – команда, которая спросит номер документа и выведет имя человека, которому он принадлежит;
-# l – list – команда, которая выведет список всех документов в формате passport "2207 876234" "Василий Гупкин";
-# s – shelf – команда, которая спросит номер документа и выведет номер полки, на которой он находится;
-# a – add – команда, которая добавит новый документ в каталог и в перечень полок, спросив его номер, тип, имя владельца
-# и номер полки, на котором он будет храниться.
 
 
 def show_person():
@@ -33,36 +23,76 @@ def list_person():
         print("{0} {1} {2}".format(doc["type"], doc["number"], doc["name"]))
 
 
-def shelf():
+def find_shelf():
     number = input("Введите номер документа:\n")
     for shelves, docs_on_shelf in directories.items():
         for doc in docs_on_shelf:
             if doc == number:
                 print("Документ c номером {0} лежит на полке {1}".format(doc, shelves))
                 break
-            else:
-                print('Документ не найден')
 
 
 def add():
     doc_type = input("Введите тип документа: ")
-    doc_number = input("Введите номер документа: ")
+    doc_number = input("Введите номер документа для удаления: ")
     doc_name = input(str("Введите имя владельца: "))
-    doc_shelf = input(str("На какую полку положить документ?"))
+    doc_shelf = input(str("На какую полку положить документ? "))
     documents.append({"type": doc_type, "number": doc_number, "name": doc_name})
+    directories[doc_shelf].append(doc_number)
     print(documents)
-    directories{"doc_shelf": doc_shelf, "number": doc_number}
+    print(directories)
 
 
-#    doc_shelf = input(int("На какую полку положить документ?"))
-#     documents["type"] = doc_type
-#     documents["number"] = doc_number
-#     documents["name"] = doc_name
-#     # directories["doc_shelf"] = doc_shelf
-#     directories["doc_number"] = doc_number
-#     # print(directories)
+def delete():
+    number = input("Введите номер документа для удаления: ")
+    for doc in documents:
+        if doc["number"] == number:
+            documents.remove(doc)
+            print(documents)
 
-# show_person()
-# list_person()
-# shelf()
-add()
+
+def move():
+    number = input("Введите номер документа для перемещения: \n")
+    new_shelf = input("Укажите полку, на которую нужно переместить документ: \n")
+    for shelves, docs_on_shelf in directories.items():
+        for doc in docs_on_shelf:
+            if doc == number:
+                docs_on_shelf.remove(doc)
+                directories[new_shelf].append(doc)
+    print(directories)
+
+
+def add_shelf():
+    shelf_name = input("Введите номер новой полки: \n")
+    directories[shelf_name] = []
+    print(directories)
+
+
+def choose_command():
+    while True:
+        command = input("Введите команду: \n"
+              "p — вывести имя владельца по номеру документа\n"
+              "l — вывести список всех документов\n"
+              "s — найти полку по номеру документа\n"
+              "a — добавить новый документ\n"
+              "d — удалить документа по его номеру\n"
+              "m — переместить документ на другую полку\n"
+              "as — создать новую полку\n")
+        if command == "p":
+            show_person()
+        elif command == "l":
+            list_person()
+        elif command == "s":
+            find_shelf()
+        elif command == "a":
+            add()
+        elif command == "d":
+            delete()
+        elif command == "m":
+            move()
+        elif command == "as":
+            add_shelf()
+        else:
+            print("Введите правильную команду")
+
+choose_command()
